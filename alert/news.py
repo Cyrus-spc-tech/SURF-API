fac47137540bab2088a2c081430374ca2e4b20c3
Tanish Gupta <tanishgupta12389@gmail.com>
2025-08-05 09:40:04 +0530

    api req

---
+++ alert/news.py
@@ -0,0 +1,4 @@
+import requests 
+
+api="019684471d894dd898639151ae78c8b6"
+base_url=f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api}"
---
+++ alert/weather.py
@@ -0,0 +1,4 @@
+import requests 
+
+api="c9d7a5418da4822623950029ce86be5e"
+base_url=f"https://api.openweathermap.org/data/2.5/weather?"
