27eb7f3839837d06ff238d93ddf92b95ac773ae2
Tanish Gupta <tanishgupta12389@gmail.com>
2025-08-09 16:43:24 +0530

    Added command for terminal sec

--- alert/cllitool.py
+++ alert/cllitool.py
@@ -1,10 +1,18 @@
 import typer 
 import rich
 from news import news
+from weather import weather
+app = typer.Typer()
 
-@app.command
-def news():
-    news()  
+
+@app.command()
+def get_news():
+    news()
+
+@app.command()
+def get_weather(city: str):
+    weather(city)
+
 
 if __name__ == "__main__":
     app()
--- alert/weather.py
+++ alert/weather.py
@@ -2,3 +2,21 @@ import requests
 
 api="c9d7a5418da4822623950029ce86be5e"
 base_url=f"https://api.openweathermap.org/data/2.5/weather?"
+
+def weather(city):
+    response = requests.get(base_url + "appid=" + api + "&q=" + city)
+    data = response.json()
+    main = data['main']
+    temperature = main['temp']
+    temprature_celcius = round(temperature - 273.15,2)
+    pressure = main['pressure']
+    humidity = main['humidity']
+    weather_description = data['weather'][0]['description']
+    print(f"City: {city}")
+    print(f"Temperature: {temprature_celcius} Celcius")
+    print(f"Pressure: {pressure} hPa")
+    print(f"Humidity: {humidity}%")
+    print(f"Weather Description: {weather_description}")
+
+if __name__ == "__main__":
+    weather("Delhi")
