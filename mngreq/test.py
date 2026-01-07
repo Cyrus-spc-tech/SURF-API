251dc29a10842ef59ed056f5569602e0baa4359b
Tanish Gupta <tanishgupta12389@gmail.com>
2026-01-07 20:28:47 +0530

    fixing file

--- mngreq/test.py
+++
@@ -1,9 +0,0 @@
-import requests 
-import json
-
-r = requests.get("https://www.pexels.com/search/bird/")
-
-try:
-    print(json.loads(r.text))
-except json.JSONDecodeError as e:
-    print(f"Error: {e}")
