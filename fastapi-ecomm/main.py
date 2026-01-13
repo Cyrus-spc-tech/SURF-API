1c796f9876b72a2be6f6ca070eafb1112f342032
Cyrus-spc-tech <vinodgupta12389@gmail.com>
2026-01-13 23:52:54 +0530

    query search

--- fastapi-ecomm/main.py
+++ fastapi-ecomm/main.py
@@ -1,8 +1,13 @@
 from app.services.products import get_all_prod
-from fastapi import FastAPI 
+from fastapi import FastAPI , HTTPException, Query
 
 app = FastAPI()
 
 @app.get("/product")
 def get_prod():
     return get_all_prod()
+
+
+@app.get("/product")
+def list_prod(name: str = Query(default = None,min_length=1,max_length=50,description="Serch ny product name : ")):
+    return name
