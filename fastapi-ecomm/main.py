718f7c67bae5b525978204957f0a1a9c6a1ae88b
Cyrus-spc-tech <vinodgupta12389@gmail.com>
2026-01-15 19:29:38 +0530

    Sort by price

--- fastapi-ecomm/main.py
+++ fastapi-ecomm/main.py
@@ -7,22 +7,47 @@ app = FastAPI()
 # def get_prod():
 #     return get_all_prod()
 
+@app.get("/")
+def root():
+    return {"msg":"Hello there how u doing "}
+
 
 @app.get("/product")
 def list_prod(
-    name: str = Query(default=None, min_length=1, max_length=50, description="Search for a product name: ")
+    name: str = Query(default=None, 
+    min_length=1, 
+    max_length=50, 
+    description="Search for a product name: "
+    ),
+    sort_by_price:bool =Query(
+        default=False,
+        description="Sort product by price "
+    ),
+    order:str = Query(default="asc",description="Sort order by (asc,desc)"),
 ):
     products = get_all_prod()
 
     if name:
         needle = name.strip().lower()
         products = [p for p in products if needle in p.get("name", "").lower()]
-
-    total = len(products)
+    
     if not products:
         raise HTTPException(
             status_code=404,
             detail=f"No product match with the name '{name}'",
         )
+
+
+    if sort_by_price:
+        reverse =order =="desc"
+        products=sorted(products,key=lambda p : p.get("price",0),reverse=reverse)
+    
+
+
+    total = len(products)
+    
+
+
+
 
     return {"total": total, "items": products}
