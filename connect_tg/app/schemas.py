b6ab6f1914a19be4724c455e9cfe47db0165247c
Cyrus-spc-tech <vinodgupta12389@gmail.com>
2026-01-18 23:59:02 +0530

    user auth

--- connect_tg/app/schemas.py
+++ connect_tg/app/schemas.py
@@ -10,3 +10,11 @@ class PostResponse(BaseModel):
     title : str
     content : str 
     
+class UserRead(schemas.BaseUser[uuid.UUID]):
+    pass
+
+class UserCreate(schemas.BaseUserCreate):
+    pass
+
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
