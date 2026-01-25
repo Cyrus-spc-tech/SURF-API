d90aba4e741ad5fb773955524ccb26dcf6904586
Cyrus-spc-tech <vinodgupta12389@gmail.com>
2026-01-25 19:12:14 +0530

    model added

--- pred_salary/backend/main.py
+++ pred_salary/backend/main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import joblib
+import numpy as np
 
 app = FastAPI()
+model = joblib.load("pred_salary_model.pkl")
 
 class ExperienceInput(BaseModel):
     exp: float
 
 @app.post("/predict")
 def predict_salary(data: ExperienceInput):
-    salary = data.exp * 5000 + 10000
+    prediction = model.predict(np.array([[data.exp]]))
     return {
         "experience": data.exp,
-        "predicted_salary": salary
+        "predicted_salary": float(prediction[0])
     }
