29b4b11e9d52f6a0d040db7cbc288f07ce8dace8
Cyrus-spc-tech <vinodgupta12389@gmail.com>
2026-03-15 09:46:39 +0530

    TEMPLATES

--- crud/uimain.py
+++ crud/uimain.py
@@ -15,52 +15,35 @@ class Item(BaseModel):
     quantity: int = 0
     description: Optional[str] = None
 
+items_db: Dict[int, Item] = {}
 
-items_db:Dict[int,Item]={}
-
-
-#create
+@app.get("/")
+def home(request:Request):
+    return templates.TemplateResponse("home.html",{"request":request})
 
 @app.post("/item/{item_id}")
-def create_item(item_id: int , item : Item):
+def create_item(item_id: int, item: Item):
     if item_id in items_db:
-        raise HTTPException(status_code=400,detail="item already exists ")
-        items_db[item_id]=item
-
-        return{ " msg " : "Item Created", "item": item}
-
-
-#read 
+        raise HTTPException(status_code=400, detail="item already exists")
+    items_db[item_id] = item
+    return {"msg": "Item Created", "item": item}
 
 @app.get("/items/{item_id}")
-def read_item(item_id : int):
+def read_item(item_id: int):
     if item_id not in items_db:
         raise HTTPException(status_code=404, detail="Item not found")
     return items_db[item_id]
-
-
-# read all
 
 @app.get("/items/")
 def read_all():
     return items_db
 
-
-
-
-# update 
-
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     if item_id not in items_db:
         raise HTTPException(status_code=404, detail="Item not found")
-    # Update the item in the dictionary
     items_db[item_id] = item
     return {"message": "Item updated", "item": item}
-
-
-
-# delete 
 
 @app.delete("/items/{item_id}")
 def delete_item(item_id: int):
@@ -68,4 +51,4 @@ def delete_item(item_id: int):
         raise HTTPException(status_code=404, detail="Item not found")
     del items_db[item_id]
     return {"message": "Item deleted"}
-
+    
