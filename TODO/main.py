6f19f677d3b281cca7c5ff17ea108e16e2967235
Cyrus-spc-tech <vinodgupta12389@gmail.com>
2026-03-15 12:35:05 +0530

    scripting frontend

--- TODO/main.py
+++ TODO/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.templating import Jinja2Templates
-from database.database import Base, engine
+from sqlalchemy.orm import Session
+from database.database import Base, engine, SessionLocal
 from routes.todo import router
+from models.model import Todo
 
 Base.metadata.create_all(bind=engine)
 
@@ -11,12 +13,22 @@ app.include_router(router, prefix="/todos", tags=["Todos"])
 # Configure templates
 templates = Jinja2Templates(directory="template")
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 # @app.get("/")
 # def read_root():
 #     return {"message": "Welcome to the Enhanced FastAPI Todo App!"}
 
-
 @app.get("/")
-def dashboard(request: Request):
-    return templates.TemplateResponse("dashboard.html", {"request": request})
+def dashboard(request: Request, db: Session = Depends(get_db)):
+    todos = db.query(Todo).all()
+    return templates.TemplateResponse("dashboard.html", {
+        "request": request, 
+        "todos": todos
+    })
