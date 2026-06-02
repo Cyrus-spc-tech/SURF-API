e3f85d95aa1db6fe328449fa8b046e56e33c7ac5
Cyrus-spc-tech <vinodgupta12389@gmail.com>
2026-06-02 22:30:50 +0530

    jjj

--- PatientReport/model/app.py
+++ PatientReport/model/app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np 
-from typing import Literal
+from typing import Literal,Annotated
 from fastapi import FastAPI,Path,Query,HTTPException,JSONResponse
 from pydantic import BaseModel,Field,computed_field
 import pickle
@@ -10,4 +10,40 @@ import pickle
 with open('model.pkl','rb') as f:
     model=pickle.load(f)
 
-app=FastAPI(    )
+app=FastAPI()
+
+
+
+# to validate incomming data
+
+class UserInput(BaseModel):
+    age:Annotated[int,Field(...,gt=0,lt=120,description="Age of the User")]
+    weight:Annotated[float,Field(...,description="weight of the User")]
+    height:Annotated[float,Field(...,description="height of the User")]
+    income:Annotated[float,Field(...,gt=0,description="Income of the User")]
+    smoker:Annotated[bool,Field(...,description="smoker or not ")]
+    city:Annotated[str,Field(...,gt=0,lt=120,description="city of the User")]
+    occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job','business_owner', 'unemployed', 'private_job'], Field( ... , description='Occupation of the user')]
+
+@computed_field
+@property
+def bmi(self)-> float :
+    return self.weigth/(self.height**2)
+
+
+@computed_field
+@property
+def life_risk(self)->str:
+    if self.smoker and self.bmi>30 :
+        return "high"
+    elif self.smoker and self.bmi >27:
+        return "medium"
+
+    else:
+        return "low"
+
+
+@computed_field
+@property
+def     
+cd 
