f055c222ce20bda3379d8d69c2c4ab2b8e60dcc2
Cyrus-spc-tech <vinodgupta12389@gmail.com>
2026-06-23 13:12:03 +0530

    coroutine

--- PatientReport/tests/asyncio.py
+++ PatientReport/tests/asyncio.py
@@ -2,9 +2,27 @@ import asyncio
 
 #coroutine fx 
 
+async def fetch_data(delay):
+    print("Fetching . . . ")
+    await asyncio.sleep(delay)
+    print("Fetched Data")
+    return {"data":"example"}
+
+
+
 async def main():
-    print("st")
-    
+    print("start of main corout:")
+    task = fetch_data(2)
+    result=await task
+
+    print(f"res:{result}")
+    print("end")
+
+
+asyncio.run(main())
+
+
+
 
 
 
